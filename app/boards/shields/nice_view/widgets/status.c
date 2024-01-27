015f71ab02ae56a12c61c6090e8fea0fbe142a0b
daniLOLZ <daniele.dente@gmail.com>
2024-01-27 18:06:37 +0100

    test art 2

--- app/boards/shields/nice_view/widgets/status.c
+++ app/boards/shields/nice_view/widgets/status.c
@@ -26,7 +26,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/keymap.h>
 #include <zmk/wpm.h>
 
-// LV_IMG_DECLARE(tails);
+LV_IMG_DECLARE(tails);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
