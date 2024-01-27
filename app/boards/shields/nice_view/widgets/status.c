91265cbd639e2d80053da5528f833529127e5feb
daniLOLZ <daniele.dente@gmail.com>
2024-01-27 15:45:24 +0100

    added tails art

--- app/boards/shields/nice_view/widgets/status.c
+++ app/boards/shields/nice_view/widgets/status.c
@@ -26,6 +26,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/keymap.h>
 #include <zmk/wpm.h>
 
+LV_IMG_DECLARE(tails);
+
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct output_status_state {
@@ -163,8 +165,6 @@ static void draw_middle(lv_obj_t *widget, lv_color_t cbuf[], const struct status
         lv_canvas_draw_text(canvas, circle_offsets[i][0] - 6, circle_offsets[i][1] - 9, 10, // i'm assuming this is the font size
                             (selected ? &label_dsc_black : &label_dsc), label);
     }
-
-    // HERE DRAW THE LOGO
 
     // Rotate canvas
     rotate_canvas(canvas, cbuf);
@@ -321,6 +321,10 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_t *bottom = lv_canvas_create(widget->obj);
     lv_obj_align(bottom, LV_ALIGN_TOP_LEFT, -44, 0); // will be aligned on the bottom and then shifted down, losing most of the canvas outside the margins
     lv_canvas_set_buffer(bottom, widget->cbuf3, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
+    
+    lv_obj_t *art = lv_img_create(widget->obj);
+    lv_img_set_src(art, &tails);
+    lv_obj_align(art, LV_ALIGN_CENTER, -50, 0);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
